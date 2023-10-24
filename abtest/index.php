c8b14819c961aaff6428ed7ec5ce6476899b6278
Peter <peterbared@live.se>
2023-10-24 15:20:32 +0200

    Nya filer

---
+++ abtest/index.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+$urlB = "sida-b.php";
+include("inc/abtest.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A/B-test</title>
+</head>
+<body>
+    <h1>Välkommen till sida A</h1>
+</body>
+</html>
